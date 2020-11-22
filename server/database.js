e2e1a73953af8b1aed110defba059a9667f25044
Alan <alanyd.phh@gmail.com>
2020-11-22 11:17:02 -0300

    Deploy

--- server/database.js
+++ server/database.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 **le dice a mongo donde buscar la base de datos
 **si esta no existe la crea
 */
-const URI = 'mongodb://localhost/Seminario'
-mongoose.connect(URI).then (db => console.log("db is connected")).catch(err => console.log(err));
+const URI = 'mongodb+srv://seminario:seminario@seminariomongodb.cwke8.mongodb.net/SeminarioMongoDB?retryWrites=true&w=majority'
+mongoose.connect(URI, {useUnifiedTopology: true,  useNewUrlParser: true}).then (db => console.log("db is connected")).catch(err => console.log(err));
 
 
 module.exports = mongoose;
